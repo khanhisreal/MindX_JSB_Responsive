72f6351cef7187383d4cc422d304486b9f542c3f
khanhisreal <nguyenhiu2k2@gmail.com>
2024-01-08 13:05:38 +0700

    first commit

---
+++ javascript/home.js
@@ -0,0 +1,42 @@
+//slides show
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName("slide");
+    let dots = document.getElementsByClassName("dot");
+    if (n > slides.length) { slideIndex = 1 }
+    if (n < 1) { slideIndex = slides.length }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+}
+
+let cart = document.getElementsByClassName("cart-products");
+
+function hover() {
+    console.log("goes hover");
+    cart[0].style.display = "block";
+}
+
+function leave() {
+    console.log("goes leave");
+    cart[0].style.display = "none";
+}
+
